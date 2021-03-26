3a0e16b9ac7d65390ea9d54b0569c2cb34671f8b
Baljinder Singh <baljinder9219@gmail.com>
2021-03-26 17:40:43 +0530

    Updated code

--- scripts/header.js
+++ scripts/header.js
@@ -1,1 +1,10 @@
-var signup_model=document.getElementsByClassName("Signup_modal");
+
+var modal = document.getElementsByClassName("Signup_modal");
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("topButtons")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal[0].style.display = "flex";
+}
