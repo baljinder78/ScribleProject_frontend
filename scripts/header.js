2c92d86e6b1774a77660a05fc1915460f8cf6587
Baljinder Singh <baljinder9219@gmail.com>
2021-03-26 19:46:14 +0530

    pop up models on click

--- scripts/header.js
+++ scripts/header.js
@@ -1,10 +1,50 @@
 
-var modal = document.getElementsByClassName("Signup_modal");
+var signup_modal = document.getElementsByClassName("Signup_modal")[0];
 
-// Get the button that opens the modal
-var btn = document.getElementsByClassName("topButtons")[0];
 
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal[0].style.display = "flex";
+var signup_btn = document.getElementsByClassName("topButtons")[0];
+
+var signup_close_btn=document.getElementsByClassName("signup_close")[0];
+
+signup_btn.onclick = function() {
+    signup_modal.style.display = "flex";
+
 }
+
+signup_close_btn.onclick=function () {
+    signup_modal.style.display="none";
+}
+
+
+
+var signin_modal = document.getElementsByClassName("Signin_modal")[0];
+
+var signin_btn = document.getElementsByClassName("topButtons")[1];
+
+var signin_close_btn=document.getElementsByClassName("signin_close")[0];
+
+signin_btn.onclick = function() {
+    signin_modal.style.display = "flex";
+
+}
+
+signin_close_btn.onclick=function () {
+    signin_modal.style.display="none";
+}
+
+
+
+
+
+
+
+
+window.onclick = function(event) {
+    if (event.target === signup_modal) {
+        signup_modal.style.display = "none";
+    }
+    if (event.target === signin_modal) {
+        signin_modal.style.display = "none";
+    }
+}
+
